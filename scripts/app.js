ea2caeaaf42b5019b3e4ccc1caf88374050eb6eb
Raquel <cruickshankrpc@gmail.com>
2020-06-04 20:51:36 +0100

    styling edits heart aliens

--- scripts/app.js
+++ scripts/app.js
@@ -26,14 +26,26 @@ function setupGame() {
     grid.appendChild(div)
     cells.push(div)
     // * grid numbers: remove at end
-    // div.innerHTML = i
+  //  div.innerHTML = i
   }
 
   // * DEFINE ALIENS 
   const aliens = [
-    17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,
-    32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42,
-    47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57
+    // 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,
+    // 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42,
+    // 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57
+
+    19, 20, 24, 25, 
+    33, 34, 35, 36, 38, 39, 40, 41,
+    47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 
+    62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 
+    72, 78, 79, 80, 81, 82, 83, 84, 85, 86, 
+    94, 95, 96, 97, 98, 99, 100,
+    110, 111, 112, 113, 114,
+    126, 127, 128,
+    142
+
+
   ]
 
   // Add aliens 
@@ -84,8 +96,9 @@ function setupGame() {
     resetGame()
   }
 
-  // function resetGame() {
-  // }
+  function resetGame() {
+    document.location.href = ""
+  }
 
   // * MOVE PLAYER 
   document.addEventListener('keydown', (event) => {
